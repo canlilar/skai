b3bc87d14e841e18542a385deb1801bb580205b0
canlilar <eden.waller@gmail.com>
2022-11-04 20:26:29 +0000

    good start

--- webApp/main.py
+++ webApp/main.py
@@ -1,6 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, render_template, Response
 import os
  
 # Flask constructor takes the name of
@@ -11,9 +11,13 @@ app = Flask(__name__)
 # which tells the application which URL should call
 # the associated function.
 @app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World This is your first app!")
-    return "Hello {}!".format(name)
+# def hello_world():
+#     name = os.environ.get("NAME", "World This is your first app!")
+#     return "Hello {}!".format(name)
+
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
  
 # main driver function
 if __name__ == "__main__":
